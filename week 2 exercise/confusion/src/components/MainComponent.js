d0860539d9f7ad9606b084179aaae4b98d89e957
zoe-zhang-seu <yanni.zhang.9710@icloud.com>
2021-02-22 12:24:17 +1100

    single page application part 2

--- week 2 exercise/confusion/src/components/MainComponent.js
+++ week 2 exercise/confusion/src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
@@ -37,6 +36,13 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
 
     return (
       <div>
@@ -44,6 +50,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />} />
               <Redirect to="/home" />
 
