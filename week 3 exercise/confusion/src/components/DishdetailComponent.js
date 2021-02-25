0be4c991ef87177b634e6c5893262fb2eb78f348
zoe-zhang-seu <yanni.zhang.9710@icloud.com>
2021-02-25 13:56:32 +1100

    moedule 3 rewrite

--- week 3 exercise/confusion/src/components/DishdetailComponent.js
+++ week 3 exercise/confusion/src/components/DishdetailComponent.js
@@ -5,6 +5,92 @@ import { Card, CardImg, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import {Control, LocalForm, Errors} from "react-redux-form";
 
+const maxLength = len => val => !val || val.length <= len;
+const minLength = len => val => val && val.length >= len;
+
+class CommentForm extends Component {
+        constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({ isModalOpen: !this.state.isModalOpen});
+    }
+
+    handleSubmit(values) { // console.log("Current State is: " + JSON.stringify(values));
+        console.log("Current State is: " + JSON.stringify(values));
+        alert("Current State is: " + JSON.stringify(values));
+    }
+
+    render(){
+      return(
+        <div>
+            <Button outline onClick={this.toggleModal}>
+                <span className="fa fa-pencil"></span>
+                Submit Comment
+            </Button>
+
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <Row className="form-group">
+                            <Label htmlFor="rating" md={2}>Rating</Label>
+                            <Col md={10}>
+                                <Control.select className="form-control" defaultValue="1" id="rating" model=".rating" name="rating">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </Control.select>
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Label htmlFor="author" md={2}> Your Name </Label>
+                            <Col md={10}>
+                                <Control.text model=".author" id="author" name="author" placeholder="Your Name" className="form-control"
+                                    validators={
+                                        {
+                                            minLength: minLength(3),
+                                            maxLength: maxLength(15)
+                                        }
+                                    }/>
+                                <Errors className="text-danger" model=".author" show="touched"
+                                    messages={
+                                        {
+                                            minLength: "Must be greater than 2 characters",
+                                            maxLength: "Must be 15 characters or less"
+                                        }
+                                    }/>
+                            </Col>
+                        </Row>
+
+                        <Row className="form-group">
+                            <Label htmlFor="message" md={2}> Comment</Label>
+                            <Col md={10}>
+                                <Control.textarea className="form-control" id="comment" model=".comment" name="comment" rows="6"/>
+                            </Col>
+                        </Row>
+
+                        <Row className="form-group">
+                            <Col md={ {size: 12}}>
+                                <Button color="primary" value="submit">Submit </Button>
+                            </Col>
+                        </Row>
+                    </LocalForm>
+                </ModalBody>
+            </Modal>
+        </div>);
+    }
+
+
+}
 
 
 
@@ -87,98 +173,6 @@ import {Control, LocalForm, Errors} from "react-redux-form";
     	}
     }
 
-const maxLength = len => val => !val || val.length <= len;
-const minLength = len => val => val && val.length >= len;
 
-class CommentForm extends Component {
-        constructor(props) {
-        super(props);
-        this.state = {
-            isModalOpen: false
-        };
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    toggleModal() {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
-    }
-
-    handleSubmit(values) { // console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
-        this.toggleModal();
-
-        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
-    }
-
-    render(){
-      return(
-        <div>
-            <Button outline onClick={this.toggleModal}>
-                <span className="fa fa-pencil"></span>
-                Submit Comment
-            </Button>
-
-            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
-                <ModalBody>
-                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
-                        <Row className="form-group">
-                            <Label htmlFor="rating" md={2}>Rating</Label>
-                            <Col md={10}>
-                                <Control.select className="form-control" defaultValue="1" id="rating" model=".rating" name="rating">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </Control.select>
-                            </Col>
-                        </Row>
-                        <Row className="form-group">
-                            <Label htmlFor="author"
-                                md={2}>
-                                Your Name
-                            </Label>
-                            <Col md={10}>
-                                <Control.text model=".author" id="author" name="author" placeholder="Your Name" className="form-control"
-                                    validators={
-                                        {
-                                            minLength: minLength(3),
-                                            maxLength: maxLength(15)
-                                        }
-                                    }/>
-                                <Errors className="text-danger" model=".author" show="touched"
-                                    messages={
-                                        {
-                                            minLength: "Must be greater than 2 characters",
-                                            maxLength: "Must be 15 characters or less"
-                                        }
-                                    }/>
-                            </Col>
-                        </Row>
-
-                        <Row className="form-group">
-                            <Label htmlFor="message" md={2}> Comment</Label>
-                            <Col md={10}>
-                                <Control.textarea className="form-control" id="comment" model=".comment" name="comment" rows="6"/>
-                            </Col>
-                        </Row>
-
-                        <Row className="form-group">
-                            <Col md={ {size: 12}}>
-                                <Button color="primary" value="submit">Submit </Button>
-                            </Col>
-                        </Row>
-                    </LocalForm>
-                </ModalBody>
-            </Modal>
-        </div>);
-    }
-
-
-}
 
 export default DishDetail;
