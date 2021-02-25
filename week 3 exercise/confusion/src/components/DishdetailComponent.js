f4558e8557b861f8fdf0f0ea0b745cfda1ce014a
zoe-zhang-seu <yanni.zhang.9710@icloud.com>
2021-02-25 13:51:30 +1100

    module 3

--- week 3 exercise/confusion/src/components/DishdetailComponent.js
+++ week 3 exercise/confusion/src/components/DishdetailComponent.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {Component}from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem  } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem ,
+    Button, Modal, ModalHeader, ModalBody,Form, FormGroup, Input, Label ,Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import {Control, LocalForm, Errors} from "react-redux-form";
+
+
 
 
     function RenderDish({dish}){
     	if (dish != null){
           return (
-            <div key={dish.id} className='col-12 col-md-5 m-1'>
+            <div key={dish.id} >
               <Card>
                   <CardImg width ="100%" src = {dish.image} alt={dish.name}/>
                   <CardBody>
@@ -42,9 +46,9 @@ import { Link } from 'react-router-dom';
         });
         
         return(
-              <div className='col-12 col-md-5 m-1'>
-              <h4>Comments </h4>
-                {thisComment}
+              <div >
+                <h4>Comments </h4>
+                  {thisComment}
               </div>
           )
     }
@@ -55,7 +59,7 @@ import { Link } from 'react-router-dom';
     			<div className="container">
 	    			  <div className="row ">
                 <Breadcrumb>
-
+                          <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
                           <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
                           <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
                 </Breadcrumb>
@@ -70,6 +74,7 @@ import { Link } from 'react-router-dom';
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <RenderComments comments={props.comments} />
+                        <CommentForm dishId={ props.dish.id } postComment={ props.postComment}/>
                     </div>
 	    			  </div>
 	    		</div>
@@ -81,5 +86,99 @@ import { Link } from 'react-router-dom';
     		)
     	}
     }
+
+const maxLength = len => val => !val || val.length <= len;
+const minLength = len => val => val && val.length >= len;
+
+class CommentForm extends Component {
+        constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) { // console.log("Current State is: " + JSON.stringify(values));
+        alert("Current State is: " + JSON.stringify(values));
+        this.toggleModal();
+
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
+    }
+
+    render(){
+      return(
+        <div>
+            <Button outline onClick={this.toggleModal}>
+                <span className="fa fa-pencil"></span>
+                Submit Comment
+            </Button>
+
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <Row className="form-group">
+                            <Label htmlFor="rating" md={2}>Rating</Label>
+                            <Col md={10}>
+                                <Control.select className="form-control" defaultValue="1" id="rating" model=".rating" name="rating">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </Control.select>
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Label htmlFor="author"
+                                md={2}>
+                                Your Name
+                            </Label>
+                            <Col md={10}>
+                                <Control.text model=".author" id="author" name="author" placeholder="Your Name" className="form-control"
+                                    validators={
+                                        {
+                                            minLength: minLength(3),
+                                            maxLength: maxLength(15)
+                                        }
+                                    }/>
+                                <Errors className="text-danger" model=".author" show="touched"
+                                    messages={
+                                        {
+                                            minLength: "Must be greater than 2 characters",
+                                            maxLength: "Must be 15 characters or less"
+                                        }
+                                    }/>
+                            </Col>
+                        </Row>
+
+                        <Row className="form-group">
+                            <Label htmlFor="message" md={2}> Comment</Label>
+                            <Col md={10}>
+                                <Control.textarea className="form-control" id="comment" model=".comment" name="comment" rows="6"/>
+                            </Col>
+                        </Row>
+
+                        <Row className="form-group">
+                            <Col md={ {size: 12}}>
+                                <Button color="primary" value="submit">Submit </Button>
+                            </Col>
+                        </Row>
+                    </LocalForm>
+                </ModalBody>
+            </Modal>
+        </div>);
+    }
+
+
+}
 
 export default DishDetail;
--- week 3 exercise/confusion/src/components/MenuComponent.js
+++ week 3 exercise/confusion/src/components/MenuComponent.js
@@ -33,8 +33,13 @@ import { Link } from 'react-router-dom';
                         <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
                         <BreadcrumbItem active>Menu</BreadcrumbItem>
                     </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr/>
+                    </div>
+
                 </div>
-                <div className="row">
+                <div className="row ">
                     {menu}
                 </div>
             </div>
