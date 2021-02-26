e1c4f28172abd24832cdb776e55b0f911cfa8fc5
zoe-zhang-seu <yanni.zhang.9710@icloud.com>
2021-02-27 00:41:57 +1100

    React Redux Forms Revisited

--- week 4 exercise/confusion/src/redux/configureStore.js
+++ week 4 exercise/confusion/src/redux/configureStore.js
@@ -1,18 +1,25 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
+import { InitialFeedback } from './forms';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+
+
 export const ConfigureStore = () => {
     const store = createStore(
-         combineReducers({
+        combineReducers({
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
