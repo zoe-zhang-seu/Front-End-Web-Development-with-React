0dd2235e1e8b0eca852bc1a452e9dcedf18157d8
zoe-zhang-seu <yanni.zhang.9710@icloud.com>
2021-02-27 00:29:52 +1100

    redux thunk

--- week 4 exercise/confusion/src/components/DishdetailComponent.js
+++ week 4 exercise/confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,8 @@ import { Card, CardImg, CardText, CardBody,
     Button, Modal, ModalHeader, ModalBody,Label ,Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {Control, LocalForm, Errors} from "react-redux-form";
+import { Loading } from './LoadingComponent';
+
 
 const maxLength = len => val => !val || val.length <= len;
 const minLength = len => val => val && val.length >= len;
@@ -147,7 +149,26 @@ class CommentForm extends Component {
     } 
 
     const DishDetail = (props) => {
-    	if (props.dish != null){
+
+       if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null){
     		return(
     			<div className="container">
 	    			  <div className="row ">
